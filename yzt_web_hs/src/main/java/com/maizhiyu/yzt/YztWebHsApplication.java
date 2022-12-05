96b6ed5697afc2bfbc3dd0c17fa8f727adf97133
26537454987072519 <1019889620@qq.com>
2022-12-05 20:14:54 +0800

    hs启动类修改

--- yzt_web_hs/src/main/java/com/maizhiyu/yzt/YztWebHsApplication.java
+++ yzt_web_hs/src/main/java/com/maizhiyu/yzt/YztWebHsApplication.java
@@ -1,12 +1,21 @@
 package com.maizhiyu.yzt;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YztWebHsApplication {
+@Slf4j
+public class YztWebHsApplication implements ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(YztWebHsApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("application start...");
+    }
 }
