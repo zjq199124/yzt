ab37fb67ecfc1dbce9e2a7d1dc69b81ebe94d5b3
26537454987072519 <1019889620@qq.com>
2023-01-04 11:40:52 +0800

    随访参数接收

--- yzt_web_hs/src/main/java/com/maizhiyu/yzt/controller/BuVisitController.java
+++ yzt_web_hs/src/main/java/com/maizhiyu/yzt/controller/BuVisitController.java
@@ -11,10 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -50,7 +47,7 @@ public class BuVisitController {
 
     @ApiOperation(value = "新增或修改随访内容", notes = "随访-新增或修改")
     @PostMapping("/updateOrInsert")
-    public Result updateOrInsert(@Validated BuVisit buVisit) {
+    public Result updateOrInsert(@Validated @RequestBody BuVisit buVisit) {
         return Result.success(buVisitService.saveOrUpdate(buVisit));
     }
 
