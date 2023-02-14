0d3c5d656c1b1ac54c977959739dcc6e68eeb045
zhangxianshao <1837377050@qq.com>
2023-02-14 17:01:47 +0800

    修改token过期的

--- yzt_his_dp/yzt_xdapi_demo/src/main/java/com/maizhiyu/yzt/feign/FeignInterceptor.java
+++ yzt_his_dp/yzt_xdapi_demo/src/main/java/com/maizhiyu/yzt/feign/FeignInterceptor.java
@@ -2,6 +2,7 @@ package com.maizhiyu.yzt.feign;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.extern.log4j.Log4j2;
 import org.apache.http.HttpHeaders;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -9,13 +10,14 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
-
+@Log4j2
 @Configuration
 public class FeignInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
         String token = FeignToken.getToken();
+        log.info("feign请求token设置为："+token);
         requestTemplate.header(HttpHeaders.AUTHORIZATION, token);
     }
 }
