8dca641c24b8dfee190abcdd95e9b7f6adcfe7a1
zhangxianshao <1837377050@qq.com>
2023-02-17 13:43:37 +0800

    修改编辑用户资料逻辑和HS登录过程

--- yzt_web_hs/src/main/java/com/maizhiyu/yzt/controller/BuOutpatientAppointmentController.java
+++ yzt_web_hs/src/main/java/com/maizhiyu/yzt/controller/BuOutpatientAppointmentController.java
@@ -8,8 +8,10 @@ import com.maizhiyu.yzt.entity.BuPrescriptionItemAppointmentItem;
 import com.maizhiyu.yzt.result.Result;
 import com.maizhiyu.yzt.ro.BuPrescriptionItemTaskRo;
 import com.maizhiyu.yzt.ro.OutpatientAppointmentRo;
+import com.maizhiyu.yzt.security.HsUserDetails;
 import com.maizhiyu.yzt.service.IBuOutpatientAppointmentService;
 import com.maizhiyu.yzt.service.IBuPrescriptionItemAppointmentItemService;
+import com.maizhiyu.yzt.service.IHsUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @Api(tags = "2.1门诊预约接口")
@@ -31,11 +35,22 @@ public class BuOutpatientAppointmentController extends BaseController {
     @Resource
     private IBuPrescriptionItemAppointmentItemService buPrescriptionItemAppointmentItemService;
 
+    @Resource
+    private IHsUserService userService;
+
     @ApiOperation(value = "查询门诊预约列表")
     @PostMapping("/list")
     public Result<List<BuOutpatientAppointment>> outpatientAppointmentList(@RequestBody OutpatientAppointmentRo outpatientAppointmentRo) {
         log.info("/*************查询待签到列表*************/");
-        Long customerId = ((Number) getClaims().get("customerId")).longValue();
+        //Long customerId = ((Number) getClaims().get("customerId")).longValue();
+        Long customerId = null;
+        HsUserDetails hsUserDetails = getHsUserDetails();
+        if (Objects.nonNull(hsUserDetails)) {
+            Map<String, Object> userMap = userService.getUser(hsUserDetails.getId());
+            if (Objects.nonNull(userMap)) {
+                customerId = Long.parseLong(userMap.get("customerId").toString());
+            }
+        }
         outpatientAppointmentRo.setCustomerId(customerId);
         Page<BuOutpatientAppointment> page = buOutpatientAppointmentService.list(outpatientAppointmentRo);
         return Result.success(page);
