67818ee9eabb8eeaf1d92a4e2c1c24a73e72da59
123 <610811603@qq.com>
2023-02-21 09:31:32 +0800

    初始权限

--- yzt_web_hs/src/main/java/com/maizhiyu/yzt/security/HsUserDetailsService.java
+++ yzt_web_hs/src/main/java/com/maizhiyu/yzt/security/HsUserDetailsService.java
@@ -1,11 +1,17 @@
 package com.maizhiyu.yzt.security;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.maizhiyu.yzt.entity.HsUserRole;
+import com.maizhiyu.yzt.mapper.HsUserRoleMapper;
+import com.maizhiyu.yzt.serviceimpl.HsUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 
 @Service
@@ -13,6 +19,12 @@ public class HsUserDetailsService implements UserDetailsService {
 
     @Autowired
     private HsUserDetailsMapper mapper;
+
+    @Resource
+    private HsUserService hsUserService;
+
+    @Resource
+    private HsUserRoleMapper hsUserRoleMapper;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -24,8 +36,17 @@ public class HsUserDetailsService implements UserDetailsService {
         // 查询用户
         HsUserDetails user = mapper.selectOne(wrapper);
 
+
+
         // 用户存在
         if (user != null) {
+            LambdaQueryWrapper wrapper1 = new LambdaQueryWrapper<>();
+            wrapper1.eq("user_id" , user.getId());
+            if (hsUserRoleMapper.selectList(wrapper1) == null){
+                HsUserRole hsUserRole = new HsUserRole();
+                hsUserRole.setUserId(user.getId()).setRoleId(3L);
+                hsUserRoleMapper.insert(hsUserRole);
+            };
             return user;
         }
 
